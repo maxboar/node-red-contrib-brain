a2de1cbf8d0db73e50819fcb11bc839aaaca305b
Randy <randypriv@gmail.com>
2015-11-23 21:24:30 +0800

    brain node

---
+++ brain/brain.js
@@ -0,0 +1,18 @@
+var brain = require('brain')
+
+module.exports = function(RED){
+  function Brain(config){
+    RED.nodes.createNode(this, config)
+    var node = this
+
+    this.on('input', function(msg){
+      var net = new brain.NeuralNetwork({
+        learningRate: node.learningRate
+      })
+      msg.payload = net
+      node.send(msg)
+    })
+  }
+
+  RED.nodes.registerType('brain', Brain)
+}
