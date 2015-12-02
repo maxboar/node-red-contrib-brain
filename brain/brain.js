1248ba0bd958d0ad7748704014a0a0d429325e09
Randy <randypriv@gmail.com>
2015-12-02 18:36:57 +0800

    seperate trainning and running

--- brain/brain.js
+++ brain/brain.js
@@ -18,46 +18,61 @@ module.exports = function(RED){
         learningRate: node.learningRate
       })
 
-      node.status({
-        fill: 'yellow',
-        shape: 'dot',
-        text: 'training'
-      })
+      if (msg.netJSON) {
+        // 不训练
+        net.fromJSON(msg.netJSON)
+        msg.payload = net.run(msg.runData)
+        node.status({
+          fill: 'green',
+          shape: 'dot',
+          text: 'running done'
+        })
+        node.send(msg)
+      } else {
+        node.status({
+          fill: 'yellow',
+          shape: 'dot',
+          text: 'training'
+        })
 
-      var trainStream = net.createTrainStream({
+        var trainStream = net.createTrainStream({
 
-        floodCallback: function() {
-          flood(trainStream, msg.trainData);
-        },
+          floodCallback: function() {
+            flood(trainStream, msg.trainData);
+          },
 
-        doneTrainingCallback: function(obj) {
-          node.status({
-            fill: 'green',
-            shape: 'dot',
-            text: 'done'
-          })
-          msg.payload = net.run(msg.runData)
-          node.send(msg)
+          doneTrainingCallback: function(obj) {
+            node.status({
+              fill: 'green',
+              shape: 'dot',
+              text: 'trainning done'
+            })
+            // msg.result = net.run(msg.runData)
+            msg.net = net.toJSON()
+            node.send(msg)
+          }
+        });
+
+        flood(trainStream, msg.trainData);
+
+        function flood(stream, data) {
+          for (var i = 0; i < data.length; i++) {
+            stream.write(data[i]);
+          }
+
+          stream.write(null);
         }
-      });
 
-      flood(trainStream, msg.trainData);
-
-      function flood(stream, data) {
-        for (var i = 0; i < data.length; i++) {
-          stream.write(data[i]);
-        }
-
-        stream.write(null);
+        // net.train(msg.trainData, {
+        //   errorThresh: config.errorThresh,
+        //   iterations: config.iterations,
+        //   log: config.log,
+        //   learningRate: config.learningRate,
+        //   logPeriod: config.logPeriod
+        // })
       }
 
-      // net.train(msg.trainData, {
-      //   errorThresh: config.errorThresh,
-      //   iterations: config.iterations,
-      //   log: config.log,
-      //   learningRate: config.learningRate,
-      //   logPeriod: config.logPeriod
-      // })
+      
 
     })
   }
